d64de01479498ab6ac66da975e77557321e27bd0
Matthew Emmanuel <manuel.fecund@gmail.com>
2024-03-03 20:40:31 +0100

    March

---
+++ public/build/assets/InputError-C8qYlHoa.js
@@ -0,0 +1,1 @@
+import{i as s,y as t,o as a,f as r,b as o,t as c}from"./app-D6KfVlxn.js";const n={class:"text-sm text-red-600"},p={__name:"InputError",props:{message:String},setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{p as _};
